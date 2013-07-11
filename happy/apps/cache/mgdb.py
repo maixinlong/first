8d6248e72fd05abd8feb0a7a0317573e85eaf8cc
mxl <maixinlong@admins-MacBook-Air-4.local>
2013-07-11 18:51:43 +0800

    存储与更新1

--- happy/apps/cache/mgdb.py
+++ happy/apps/cache/mgdb.py
@@ -8,33 +8,50 @@ author:xiaomai
 
 import pymongo
 import bson
+import datetime
+
+
+
 ##bson.objectid.ObjectId(unicode(value))
 con = pymongo.Connection()
 db = con.datas
 db2 = con.datas_key
+db_ver = con.datas_ver
+today = str(datetime.date.today())
 
 
-def save(types,datas):
+def save(types,data):
     """
     @params types:微博类型
-    @params datas:{'aa':[]}
+    @params data:{评论标记，微博内容，提交日期}
+	data = {'comment':'','weibo':{},'date':'07-10'}
     """
-    obj = ''
-    num = 0 
-    if types in ['fine','video','amuse','dynamic']:
+    if not types or not data:
+        return
+    if types not in ['fine','video','amuse','dynamic']:
+        return
+    #datas = {'07-10':[data1,data2]}
+    datas = db[types].find_one({'date':today})[0]
+    if datas:
+		datas.update({'time':today},{'$push':{'datas':data}})
+    else:
+        datas = {'time':today,'datas':data}
         obj = db[types].insert(datas)
+    #版本号更新
+    set_ver(types):
     print 'save success...'
 
 
 def get(num=-1,types=None,new=False):
     """
     @params num:num 越大表示数据越新
-    ＠params types:微博类型
+    @params types:微博类型
     @return :datas,next_num
     """
     datas = {}
     if types not in ['fine','video','amuse','dynamic']:
         return datas,-2
+
     count = db[types].find().count()
     num = int(num)
     if num >= count or count <= 0:
@@ -44,11 +61,68 @@ def get(num=-1,types=None,new=False):
         datas = db[types].find().skip(count-1)[0]
 	return datas,count-1
 
-    if num < 0:
+    if num <= 0:
         return datas,num
     else:
         datas = db[types].find().skip(num)[0]
     return datas,num-1
+
+def get_update_first():
+    """
+	初次加载
+	"""
+    datas = db[types].find_one({'time':today})
+    if datas:
+        datas = datas[0]['datas']
+    return datas,len(datas)
+
+def get_update_today(num,types):
+    """
+	同一天内更新哦数据
+    """
+    datas = db[types].find_one({'time':today})
+    if datas:
+        datas = datas[0]['datas'][num:]
+    else:
+        return [],num
+	return datas,num+len(datas)
+    
+def get_udate_yestoday(t,num,types):
+    """
+	跨天更新昨天加今天
+	@params t:时间
+	@params num:当时更新条数
+	@return :datas,本次更新条数
+	"""
+    datas = db[types].find_one({'time':t})
+    if not datas:
+        return [],num
+    datas = datas[0]['datas'][num:]
+	#昨天与今天数据一起
+	today_datas,today_num = get_update(0,types)
+	if today_datas:
+        datas += today_datas
+		return datas,today_num
+	return datas,len(datas)
+
+
+def set_ver(types):
+    ver_config = db_ver[types].find_one({'type':types})
+    t = str(datetime.date.today())
+    if ver_config:
+        ver = ver_config['ver'].split("+")[1]
+        ver_config.update({'type':types},{'$set':{'ver': "%s+%d" % (t,ver+1)}})
+    else:
+        db_ver[types].insert({'type':types,'ver':"%s+%d" % (t,0)})
+	print 'set_ver',db_ver[types].find_one({'type':types})
+
+def get_ver(types):
+    ver = db_ver[types].find_one({'type':types})
+    if ver:
+        ver = ver[0]['ver']
+    else:
+        ver = "%s+%d" % (str(datetime.date.today()),0)
+    return ver
 
 
 if __name__ == "__main__":
--- happy/apps/views/content.py
+++ happy/apps/views/content.py
@@ -4,67 +4,59 @@ import redis
 import json
 import pickle
 import datetime
-
 from apps.common.http_util import render,render_text
+from apps.cache import mgdb
 
+DEBUG = False
 rd = redis.Redis(host='localhost', port=6379, db=1)
-
-def get_content1111111(request):
-    page = int(request.GET.get('page',1))
-    types = request.GET.get('type',0)
-    
-    datas = rd.get('liuxin_test')
-    datas = pickle.loads(datas)
-    datas = datas[:page*20]
-    
-    if types == "dynamic":
-        datas = rd.get('liuxin_gif')
-        datas = pickle.loads(datas)
-        datas = datas[:page*20]
-        datas.sort(key=lambda a:(a.get('file_size')), reverse=False)
-    
-    
-    
-    #data = json.dumps(,sort_keys=True,indent=4, separators=(',', ': '))
-    data = json.JSONEncoder().encode( {'num':20,'list':datas} )
-    print data
-    return render_text(data)
-    
+ver2 = "001+15"
 
 def update_content(request):
-    types = request.GET.get('type','fine')
-    last_datas = []
-    new_datas = []
-    if types not in ['dynamic','fine']:
-        types = 'fine'
-    new_datas = rd.get(types)
-    #新旧数据比对、新数据与老数据是否累计？
-    #
+    """
+	往下拉更新数据
+	"""
+    types = request.GET.get('type')
+    ver1 = request.GET.get('ver')
+    ver2 = mgdb.get_ver()
+    if types not in ['dynamic','fine','video']:
+        return {}
 
-    datas = pickle.loads(new_datas)[:20]
-    if not datas:
-        datas = last_datas
-    last_id = datas[-1]['id']
-    data = json.JSONEncoder().encode( {'last_id':last_id,'count':20,'list':datas,'update_time':'2013-06-27'})
+    if ver1 == ver2:
+        return {}
+    #同一天更新
+    if ver1.split("+")[0] == ver2.split("+")[0]:
+        datas = get_update(num,types)
+	else:
+        pass
+	client_num = int(ver1)
+	db_num = int(ver2)
+
+	update_num = db_num - client_num
+    if update_num <= 0:
+        return {}
+    #初次加载
+    datas = mgdb.get(new=True)
+    data = json.JSONEncoder().encode( {'ver':ver2,'count':update_num,'list':datas})
     return render_text(data)
 
 
 def get_content(request):
-    types = request.GET.get('type','fine')
-    #last_id 得初次加载时传上去
-    last_id = request.GET.get('last_id',0)
-    
-    if types not in ['fine','dynamic']:
-        types = 'fine'
-    datas = rd.get(types)
-    datas = pickle.loads(datas)
-    #比较上一条位置，往后接着取
-    for i in datas:
-        if i['id'] == last_id:
-            index = datas.index(i)
-            datas = datas[index+1:index+20]
-            break
-    last_id = datas[-1]['id']
-    data = json.JSONEncoder().encode( {'last_id':last_id,'list':datas} )
-    print data
+    """
+    往上拉读取旧数据
+	"""
+    types = request.GET.get('type')
+    next_num = request.GET.get('num')
+
+    datas,next_num = mgdb.get(num=next_num,types=types,new=False)
+    ver = mgdb.get_ver()
+    if not datas:
+        return {}
+    datas = json.JSONEncoder().encode({'list':datas,'ver':ver,'next_num':next_num})
     return render_text(data)
+
+
+   
+
+
+
+
